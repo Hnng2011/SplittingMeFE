3db1f8d84308c325677b4f4975646e1949c62ee9
Hnng2011 <luphihung111@gmail.com>
2023-09-07 20:21:00 +0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Dashboard from "./Dashboard";
 import Explore from "./Explore02";
 import Home from "./Home";
 import Ranking from "./Ranking";
-import Wallet from "./Wallet";
 import Pool from "./pool";
 import ItemDetails from "./ItemDetails";
 import Mint from "./mint";
@@ -18,7 +17,6 @@ const routes = [
   { path: '/marketplace', component: <Explore /> },
   { path: '/dashboard', component: <Dashboard /> },
   { path: '/referal', component: <Ranking /> },
-  { path: '/wallet', component: <Wallet /> },
   { path: '/contact', component: <Contact /> },
   { path: '/item-details', component: <ItemDetails /> },
   { path: '/mint', component: <Mint /> },
